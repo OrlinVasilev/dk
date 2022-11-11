c8e49a12a24c58e48f0a3c2f9123ca4e62ec0168
OrlinVasilev <ovasilev@vmware.com>
2022-11-11 13:10:08 +0200

    add first 2 tasks

---
+++ 2/app.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type App struct {
+	Port string
+}
+
+func (a *App) Start() {
+	http.Handle("/", logreq(index))
+	addr := fmt.Sprintf(":%s", a.Port)
+	log.Printf("Starting app on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func env(key, defaultValue string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return val
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	var imageName string
+	imageName = os.Getenv("IMAGE")
+	fmt.Fprintf(w, "hello world from image: app:%s\n", imageName)
+}
+
+func logreq(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("path: %s", r.URL.Path)
+
+		f(w, r)
+	})
+}
+
+func main() {
+	server := App{
+		Port: env("PORT", "8080"),
+	}
+	server.Start()
+}
